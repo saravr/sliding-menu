38a771b678eda4dcc6fdf8a5bf2dfaa1361755bf
Sarav Ramaswamy <sarav@verizonmedia.com>
2021-07-06 19:38:57 -0700

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
-import Menu from './Menu.js';
+//import React from 'react'
+//import { Container, Navbar, NavbarBrand } from 'react-bootstrap'
+import Menu from './Menu.js'
+
+/*class AppFooter extends React.Component {
+  render() {
+      return(
+          <div className="fixed-bottom">  
+              <Navbar color="dark" dark>
+                  <Container>
+                      <NavbarBrand>Footer</NavbarBrand>
+                  </Container>
+              </Navbar>
+          </div>
+      )
+  }
+}*/
 
 function App() {
   return (
